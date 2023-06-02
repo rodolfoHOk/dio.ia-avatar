0f74269128b7e41513f479c2c2fecf8e30f45945
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-06-02 00:01:34 -0300

    add infrastructure rest service

---
+++ quarkus-app/src/main/java/br/com/hioktec/infrastructure/rest/services/StableDiffusionService.java
@@ -0,0 +1,33 @@
+package br.com.hioktec.infrastructure.rest.services;
+
+import br.com.hioktec.domain.models.ProfilePhoto;
+import br.com.hioktec.infrastructure.rest.clients.StableDiffusionRestClient;
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.NoSuchElementException;
+
+@ApplicationScoped
+public class StableDiffusionService {
+  private final StableDiffusionRestClient stableDiffusionRestClient;
+
+  public StableDiffusionService(StableDiffusionRestClient stableDiffusionRestClient) {
+    this.stableDiffusionRestClient = stableDiffusionRestClient;
+  }
+
+  @Transactional
+  public Uni<String> generate(ProfilePhoto profilePhoto) throws IOException {
+    var fileContent = FileUtils.readFileToByteArray(new File(profilePhoto.originalPhoto()));
+    var encodedString = Base64.getEncoder().encodeToString(fileContent);
+
+    return stableDiffusionRestClient
+      .img2img(new StableDiffusionRestClient.StableDiffusionRequest(encodedString))
+      .onItem()
+      .transform(response -> response.images().stream().findFirst().orElseThrow(NoSuchElementException::new));
+  }
+}
