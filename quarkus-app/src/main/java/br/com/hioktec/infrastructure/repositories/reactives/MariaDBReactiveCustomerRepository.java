d00939f74142a643db0e8bd02f3abe84a77c6f4e
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-06-02 22:25:27 -0300

    reactive consumer repository now group consumer profile photos

--- quarkus-app/src/main/java/br/com/hioktec/infrastructure/repositories/reactives/MariaDBReactiveCustomerRepository.java
+++ quarkus-app/src/main/java/br/com/hioktec/infrastructure/repositories/reactives/MariaDBReactiveCustomerRepository.java
@@ -11,6 +11,8 @@ import io.vertx.mutiny.sqlclient.RowSet;
 import io.vertx.mutiny.sqlclient.Tuple;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.Map;
+
 @ApplicationScoped
 public class MariaDBReactiveCustomerRepository implements ReactiveCustomerRepository {
 
@@ -22,9 +24,28 @@ public class MariaDBReactiveCustomerRepository implements ReactiveCustomerReposi
 
   @Override
   public Multi<Customer> findAll() {
-    Uni<RowSet<Row>> rowSet = client.query("SELECT * FROM profile_photos").execute();
+    Uni<RowSet<Row>> rowSet = client
+      .query("SELECT * FROM profile_photos")
+      .execute();
 
-    Multi<CustomerProfilePhotos> customersProfilePhotos = rowSet
+    var customersProfilePhotos = hydrateMultiCustomerProfilePhotos(rowSet);
+
+    return convertToMultiConsumer(customersProfilePhotos);
+  }
+
+  @Override
+  public Uni<Customer> findById(String id) {
+    Uni<RowSet<Row>> rowSet = client
+      .preparedQuery("SELECT * FROM profile_photos WHERE customer_id = ?")
+      .execute(Tuple.of(id));
+
+    var customersProfilePhotos = hydrateMultiCustomerProfilePhotos(rowSet);
+
+    return convertToMultiConsumer(customersProfilePhotos).collect().first();
+  }
+
+  private static Multi<CustomerProfilePhotos> hydrateMultiCustomerProfilePhotos(Uni<RowSet<Row>> rowSet) {
+    return rowSet
       .onItem().transformToMulti(set -> Multi.createFrom().iterable(set))
       .onItem().transform(row -> {
         CustomerProfilePhotos customer = new CustomerProfilePhotos();
@@ -35,33 +56,18 @@ public class MariaDBReactiveCustomerRepository implements ReactiveCustomerReposi
         customer.setGeneratedPhoto(row.getString("generated_photo"));
         return customer;
       });
-
-    return customersProfilePhotos
-      .onItem().transform(CustomerProfilePhotos::toDomain);
   }
 
-  @Override
-  public Uni<Customer> findById(String id) {
-    Uni<RowSet<Row>> rowSet = client
-      .preparedQuery("SELECT * FROM profile_photos WHERE customer_id = ?")
-      .execute(Tuple.of(id));
-
-    Uni<CustomerProfilePhotos> customerProfilePhotos = rowSet
-      .onItem().transform(RowSet::iterator)
-      .onItem().transform(iterator -> {
-        if (iterator.hasNext()) {
-          var row = iterator.next();
-          var customer = new CustomerProfilePhotos();
-          customer.setCompositeKey(new CustomerProfilePhotos.CompositeKey());
-          customer.getCompositeKey().setCustomerId(row.getString("customer_id"));
-          customer.getCompositeKey().setId(row.getString("id"));
-          customer.setOriginalPhoto(row.getString("original_photo"));
-          customer.setGeneratedPhoto(row.getString("generated_photo"));
-          return customer;
-        }
-        return null;
-      });
-
-    return customerProfilePhotos.onItem().transform(CustomerProfilePhotos::toDomain);
+  private static Multi<Customer> convertToMultiConsumer(Multi<CustomerProfilePhotos> customersProfilePhotos) {
+    return customersProfilePhotos
+      .onItem().transform(CustomerProfilePhotos::toDomain)
+      .collect().asMultiMap(Customer::id)
+      .map(Map::entrySet)
+      .onItem().transform(set -> set.stream()
+        .map(entry -> new Customer(
+          entry.getKey(),
+          entry.getValue().stream().flatMap(customer -> customer.profilePhotos().stream()).toList()))
+        .toList())
+      .onItem().transformToMulti(set -> Multi.createFrom().iterable(set));
   }
 }
